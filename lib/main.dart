31e846a8fcbe1341ac909d6b0865bffc811c6e62
dhavalkhant3-web <dhavalkhant3@gmail.com>
2026-09-20 16:34:28 +0530

    feat: initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(const GsetApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const GsetApp());
+}
 
 class GsetApp extends StatelessWidget {
   const GsetApp({super.key});
